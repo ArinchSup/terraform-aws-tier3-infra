455ab76c65ce4a930798e5a0c147bf85f7c9a576
ArinchSup <arinchwork@gmail@gmail.com>
2026-05-16 21:11:06 +0700

    vpc in progress

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,21 @@ variable "aws_region" {
   type = string
   default = "ap-southeast-1"
 }
+
+variable "project_name" {
+  description = "the project name"
+  type = string
+  default = "tier3"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type = string
+  default = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type = string
+  default = "10.0.0.0/16"
+}
