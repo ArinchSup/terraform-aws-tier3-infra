72cbd034e0c752246d0f3ec0520a314bace09b9a
PaleCanary <arinarin200611@gmail.com>
2026-05-17 15:19:00 +0700

    vpc updated

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,12 +3,20 @@ terraform {
 
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~>5.0"
+      source  = "hashicorp/aws"
+      version = "~>5.0"
     }
   }
 }
 
 provider "aws" {
   region = var.aws_region
+
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Project     = var.project_name
+      ManagedBy   = "terraform"
+    }
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,23 @@
 variable "aws_region" {
   description = "any region (ap-southeast-1 (Singapore) for now)"
-  type = string
-  default = "ap-southeast-1"
+  type        = string
+  default     = "ap-southeast-1"
 }
 
 variable "project_name" {
   description = "the project name"
-  type = string
-  default = "tier3"
+  type        = string
+  default     = "tier3"
 }
 
 variable "environment" {
   description = "Deployment environment"
-  type = string
-  default = "dev"
+  type        = string
+  default     = "dev"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
-  type = string
-  default = "10.0.0.0/16"
+  type        = string
+  default     = "10.0.0.0/16"
 }
