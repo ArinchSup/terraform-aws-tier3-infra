bcf3abc5388b6f559a34971cc37195c3ce7c6847
PaleCanary <arinarin200611@gmail.com>
2026-05-18 09:58:00 +0700

    feat: route table and subnets

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,3 +21,21 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_app_subnet_cidrs" {
+  description = "CIDR blocks for private apps"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "private_db_subnet_cidrs" {
+  description = "CIDR blocks for private db"
+  type        = list(string)
+  default     = ["10.0.21.0/24", "10.0.22.0/24"]
+}
