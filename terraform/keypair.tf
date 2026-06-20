5a65d235c51682582a87c9f092ac4d5a28044222
PaleCanary <arinarin200611@gmail.com>
2026-06-20 14:44:36 +0700

    keypair

--- terraform/keypair.tf
+++ terraform/keypair.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "main" {
   key_name   = "${var.project_name}-${var.environment}-key"
-  public_key = file(pathexpand(var.ssh_public_key_path))
+  public_key = file("${path.module}/terraform_3tier.pub")
   tags = {
     Name = "${var.project_name}-${var.environment}-key"
   }
